5c9e0916b412900e69a2aa5cb0d177e85c67ea15
reed <hsfarid08@gmail.com>
2024-07-28 23:12:49 +0000

    modify alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,7 +29,7 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE OrderDetails (
+CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
